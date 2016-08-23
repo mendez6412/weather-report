02cb4b5ed64fda1cb0e41c1f555ee6f9cc8abcb3
mendez6412 <mendez6412@gmail.com>
2016-08-22 23:41:03 -0400

    added tests

--- weather_test.py
+++ weather_test.py
@@ -1,5 +1,23 @@
-from weather import *
+from weather import Weather
 
 
 def test():
-    assert True == True
+    dc = Weather(20002)
+
+    assert dc.zipcode == 20002
+
+
+def test_fake_lookup():
+    dc = Weather(20002)
+    dcweather = dc.lookup(test=1)
+    
+    assert dc.result['current_observation']['display_location']['city'] == 'Durham' 
+
+
+# def test_real_lookup():
+    """ Do not run this all the time """
+    # dc = Weather(20002)
+    # dcweather = dc.lookup()
+
+    # assert dc.result['current_observation']['display_location']['city'] == 'Washington'
+
